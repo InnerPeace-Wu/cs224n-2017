228b45d8998f464b21648caf6167e7b33a02bff1
InnerPeace-Wu <innerpeace.wu@outlook.com>
2017-09-11 21:53:03 +0800

    update a2 q1

--- assignment2/q1_softmax.py
+++ assignment2/q1_softmax.py
@@ -24,6 +24,8 @@ def softmax(x):
     """
 
     ### YOUR CODE HERE
+    x -= tf.reduce_max(x, axis = 1, keep_dims=True)
+    out = tf.exp(x) / tf.reduce_sum(tf.exp(x),axis = 1, keep_dims=True)
     ### END YOUR CODE
 
     return out
@@ -54,6 +56,7 @@ def cross_entropy_loss(y, yhat):
     """
 
     ### YOUR CODE HERE
+    out = - tf.reduce_sum(tf.to_float(y) * tf.log(yhat))
     ### END YOUR CODE
 
     return out
